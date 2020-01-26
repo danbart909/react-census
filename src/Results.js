d494c3cc39fc50a4661a9aaadc3eb3d734f67ecb
Dan Bartlett <danbart909@gmail.com>
2020-01-26 16:32:47 -0500

    used React for checkboxes

--- src/Results.js
+++ src/Results.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react'
 import $ from 'jquery'
 
 export default class Results extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      resultsContainer1: false,
+      resultsContainer2: false,
+      resultsContainer3: false,
+      resultsContainer4: false,
+      resultsContainer5: false
+    }
+  }
 
 	states1 = () => {
     let tableStates1 = (
@@ -78,9 +88,13 @@ export default class Results extends Component {
       );
     }
     if (this.props.state.labels.race.length !== 0) {
-      $(".resultsContainer1").show();
+      this.setState({
+        resultsContainer1: true
+      })
     } else {
-      $(".resultsContainer1").hide();
+      this.setState({
+        resultsContainer1: false
+      })
     }
     let tableRaces = (
       <table>
@@ -109,10 +123,14 @@ export default class Results extends Component {
       );
     }
 		if (this.props.state.labels.size.length !== 0) {
-			$('.resultsContainer2').show()
-			} else {
-			$('.resultsContainer2').hide()
-      }
+			this.setState({
+        resultsContainer2: true
+      })
+    } else {
+      this.setState({
+        resultsContainer2: false
+      })
+    }
     let tableSizes = (
       <table>
         <tr>
@@ -140,9 +158,13 @@ export default class Results extends Component {
       );
     }
 		if (this.props.state.labels.ASM.length !== 0) {
-			$('.resultsContainer3').show()
-			} else {
-			$('.resultsContainer3').hide()
+			this.setState({
+        resultsContainer3: true
+      })
+    } else {
+      this.setState({
+        resultsContainer3: false
+      })
     }
     let tableASM = (
       <table>
@@ -171,10 +193,14 @@ export default class Results extends Component {
       );
     }
 		if (this.props.state.labels.ASF.length !== 0) {
-			$('.resultsContainer4').show()
-			} else {
-			$('.resultsContainer4').hide()
-      }
+			this.setState({
+        resultsContainer4: true
+      })
+    } else {
+      this.setState({
+        resultsContainer4: false
+      })
+    }
     let tableASF = (
       <table>
         <tr>
@@ -197,26 +223,26 @@ export default class Results extends Component {
 	render() {
 		return (
 			<>
-				<div className="resultsContainer1">
+				{this.state.resultsContainer1 ? <div className="resultsContainer1">
 					<div className="resultsStates1">{this.states1()}</div>
 					<div className="resultsRaces">{this.resultsRace()}</div>
-				</div>
-				<div className="resultsContainer2">
+				</div> : ''}
+				{this.state.resultsContainer2 ? <div className="resultsContainer2">
 					<div className="resultsStates2">{this.states2()}</div>
 					<div className="resultsSizes">{this.resultsSizes()}</div>
-				</div>
-				<div className="resultsContainer3">
+				</div> : ''}
+				{this.state.resultsContainer3 ? <div className="resultsContainer3">
 					<div className="resultsStates3">{this.states3()}</div>
 					<div className="resultsASM">{this.resultsASM()}</div>
-				</div>
-				<div className="resultsContainer4">
+				</div> : ''}
+				{this.state.resultsContainer4 ? <div className="resultsContainer4">
 					<div className="resultsStates4">{this.states4()}</div>
 					<div className="resultsASF">{this.resultsASF()}</div>
-				</div>
-				<div className="resultsContainer5">
+				</div> : ''}
+				{this.state.resultsContainer5 ? <div className="resultsContainer5">
 					<div className="resultsStates5"></div>
 					<div className="resultsHousehold"></div>
-				</div>
+				</div> : ''}
 				{/* <button onClick={console.log('')}>generate</button> */}
 			</>
 		)
